d6e9628ab6de5b133d3351e21dbc28a8631a5861
M Iftikhar Uddin Khan Sami <hits89411a@gmail.com>
2020-12-23 14:14:57 +0500

    miks

--- Cipher Framework/src/cipher/framework/CipherFramework.java
+++ Cipher Framework/src/cipher/framework/CipherFramework.java
@@ -19,26 +19,48 @@ public class CipherFramework {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
+        System.out.println("==============================================");
         System.out.println("Please Select the Cipher technique you want to run");
         System.out.println("Press 1 for PlayFair Cipher");
         System.out.println("Press 2 for Hill Cipher");
         System.out.println("\n\n Enter your option: ");
         int val = sc.nextInt();
         
+        System.out.println("==============================================");
         if (val == 1) {
-        Cipher_Interface cf = new Playfair_cipher();
+        Playfair_cipher pf = new Playfair_cipher();
+        System.out.println("==============================================");
         System.out.println("Enter Keyword");
-        String k =sc.next();
-        pf.setKey(k);
+        String pk =sc.next();
+        pf.setKey(pk);
         pf.generate_key();
+        System.out.println("==============================================");
+        System.out.println("Enter Text to Encrypt:  ");
+        String text_pe =sc.next();
+        String pe =pf.encrypt(text_pe);
+        System.out.println("The Encrypted Text is   "+pe);
+        System.out.println("==============================================");
+        System.out.println("Enter Text to Decrypt");
+        String text_pd =sc.next();
+        String pd = pf.decrypt(text_pd);
+        System.out.println("The Decrypted Text is    "+pd);
+        System.out.println("==============================================");
+        }
+      /*  else if (val == 2) {
+        
+        
+        System.out.println("Enter Keyword");
+        String hk =sc.next();
+       // hf.setKey(hk);
+        hf.generate_key();
         System.out.println("Enter Text to Encrypt");
-        String text_e =sc.next();
-        pf.encrypt(text_e);
-        pf.decrypt();
-        }
-        else if (true) {
-            
-        }
+        String text_he =sc.next();
+        hf.encrypt(text_he);
+        
+        System.out.println("Enter Text to Decrypt");
+        String text_hd =sc.next();
+        hf.decrypt(text_hd);
+        }*/
         else{
             System.out.println("Press any key to Exit");
         }
