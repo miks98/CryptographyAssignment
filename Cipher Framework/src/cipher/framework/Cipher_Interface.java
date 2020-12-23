83deaed5c09bdfd8fa7e2d4608af2e81f97e48b5
M Iftikhar Uddin Khan Sami <hits89411a@gmail.com>
2020-12-23 12:25:16 +0500

    cryptography assignment

---
+++ Cipher Framework/src/cipher/framework/Cipher_Interface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cipher.framework;
+
+/**
+ *
+ * @author Omer & MIKS
+ */
+public interface Cipher_Interface {
+    
+    
+public  void generate_key();
+public  void encrypt();
+public  void decrypt();
+public  void cryptoanalysis();
+
+
+    
+}
---
+++ Cipher Framework/src/cipher/framework/playfair_cypher.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cipher.framework;
+
+/**
+ *
+ * @author Omer & MIKS
+ */
+public class playfair_cypher implements Cipher_Interface {
+
+    @Override
+    public void generate_key() {
+        
+    }
+
+    @Override
+    public void encrypt() {
+        
+    }
+
+    @Override
+    public void decrypt() {
+        
+    }
+
+    @Override
+    public void cryptoanalysis() {
+        
+    }
+    
+}
